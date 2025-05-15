81c560cfff54024a22f095e5116ddabf6ed7351c
oisnard <isnard@free.fr>
2025-05-15 15:58:05 +0200

    Update of model_train.py

---
+++ src/models/model_train.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import joblib
+import pickle
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+# Load the training data    
+X_train_scaled = pd.read_csv('data/processed_data/X_train_scaled.csv', index_col=0)
+y_train = pd.read_csv('data/processed_data/y_train.csv', index_col=0) 
+
+
+# Load the best parameters
+with open('models/best_params.pkl', 'rb') as f:
+    best_params = pickle.load(f)
+
+
+# Define the model with the best parameters 
+model = GradientBoostingRegressor(**best_params)
+# Fit the model to the training data
+model.fit(X_train_scaled, y_train.values.ravel())
+# Save the model to a file
+joblib.dump(model, 'models/model.pkl')
