f32e005641bc117c1f91f0abe7621c8987de9850
oisnard <isnard@free.fr>
2025-05-15 13:26:18 +0200

    Rename data_split.py to make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,22 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split 
+import os 
 
-data = pd.read_csv('data/raw_data/raw.csv', index_col=0)
+# Check if the directory exists, if not create it
+if os.path.isfile('data/raw_data/raw.csv'):
+    data = pd.read_csv('data/raw_data/raw.csv', index_col=0)
+else:
+    url = "https://datascientest-mlops.s3.eu-west-1.amazonaws.com/mlops_dvc_fr/raw.csv"
+    data = pd.read_csv(url, index_col=0)
+    data.to_csv('data/raw_data/raw.csv')
 
+# Extract the features and target variable
+# The target variable is 'silica_concentrate' and the features are all other columns
 data_features = data.drop(columns=['silica_concentrate'])
 data_target = data['silica_concentrate']
+
+# Define the ratio for the train-test split
+# 70% of the data will be used for training and 30% for testing
 RATIO_TEST = 0.3
 
 
